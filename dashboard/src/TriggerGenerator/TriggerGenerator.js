1c429178f93c1b40dc866942ff52496b2f6c21cf
Nicolas Felten <nicolas.felten@epitech.eu>
2020-03-19 18:07:34 +0100

    Finition trigger

--- dashboard/src/TriggerGenerator/TriggerGenerator.js
+++ dashboard/src/TriggerGenerator/TriggerGenerator.js
@@ -5,17 +5,32 @@ export default class TriggerGenerator extends Component {
     super(props);
     this.state = {
       value : 0,
-      trigger : "",
-      triggeringFonction : "",
-      TriggeredFonction : ""
+      trigger : ">",
+      triggeringFonction : "temperature",
+      TriggeredFonction : "light",
+      TriggeredValueClassName : "",
+      argument : "",
+      lightArgClass : "",
+      socketArgClass : "d-none",
+      ledmessengerArgClass : "d-none",
+      captorClass : "form-group text-center",
+      lightsClass : "form-group text-center d-none",
     }
     this.FillTriggerList = this.FillTriggerList.bind(this);
     this.updateTriggerValue = this.updateTriggerValue.bind(this);
     this.addTrigger = this.addTrigger.bind(this);
-    this.fillTemperatureTrigger = this.fillTemperatureTrigger.bind(this);
-    this.fillLightTrigger = this.fillLightTrigger.bind(this);
+    
+    this.updateChangeValue = this.updateChangeValue.bind(this);
+    
     this.updateTrigger = this.updateTrigger.bind(this);
     this.updateTriggeredFunction = this.updateTriggeredFunction.bind(this);
+
+    /* ELEMENTS REF */
+    this.tempRef = React.createRef();
+  }
+
+  updateChangeValue(e) {
+    this.setState({argument: e.target.value});
   }
 
   updateTrigger(e) {
@@ -25,6 +40,25 @@ export default class TriggerGenerator extends Component {
 
   updateTriggeredFunction(e) {
     this.setState({TriggeredFonction:e.target.value});
+    if (e.target.value === "light") {
+      this.setState({
+        lightArgClass : "form-group text-center",
+        socketArgClass : "form-group text-center d-none",
+        ledmessengerArgClass : "form-group text-center d-none"
+      })
+    } else if (e.target.value === "socket") {
+      this.setState({
+        lightArgClass : "form-group text-center d-none",
+        socketArgClass : "form-group text-center",
+        ledmessengerArgClass : "form-group text-center d-none"
+      })
+    } else { //e.target.value == ledmessenger
+      this.setState({
+        lightArgClass : "form-group text-center d-none",
+        socketArgClass : "form-group text-center d-none",
+        ledmessengerArgClass : "form-group text-center"
+      })
+    }
   }
 
   addTrigger(e) {
@@ -32,6 +66,7 @@ export default class TriggerGenerator extends Component {
     console.log("trigger = ", this.state.trigger);
     console.log("triggeringFunction = ", this.state.triggeringFonction);
     console.log("triggeredFonction = ", this.state.TriggeredFonction);
+    console.log("argument =", this.state.argument);
   }
 
   componentDidMount() {
@@ -45,8 +80,15 @@ export default class TriggerGenerator extends Component {
   getTriggerList() {
     var triggeringFonctions = [];
 
-    triggeringFonctions.push(this.fillTemperatureTrigger);
-    triggeringFonctions.push(this.fillLightTrigger);
+    triggeringFonctions.push("temperature");
+    triggeringFonctions.push("humidity");
+    triggeringFonctions.push("pressure");
+    triggeringFonctions.push("magnetometer");
+    triggeringFonctions.push("gyroscope");
+    triggeringFonctions.push("accelerometer");
+    triggeringFonctions.push("light");
+    triggeringFonctions.push("socket");
+
 
     return (triggeringFonctions);
   }
@@ -58,24 +100,30 @@ export default class TriggerGenerator extends Component {
     var value = options[options.selectedIndex].value;
     console.log(value);
 
-    
-
     var triggeringFonctionsList = this.getTriggerList();
-    triggeringFonctionsList[index]();
-  }
 
+    this.setState({triggeringFonction : triggeringFonctionsList[index]});
+    console.log("Triggering fonction", this.state.triggeringFonction);
 
-  fillTemperatureTrigger() {
-    this.setState({triggeringFonction : "temperature"});
-    console.log("Température");
-  }
-
-  fillLightTrigger() {
-    this.setState({triggeringFonction : "light"});
-    console.log("Light");
+    if (value <= 5) {
+      console.log("WTF");
+        this.setState({
+          captorClass : "form-group text-center",
+          lightsClass : "form-group text-center d-none",    
+        });
+    } else { //Lumière ou interrupteur
+      this.setState({
+        captorClass : "form-group text-center d-none",
+        lightsClass : "form-group text-center"  
+      });
+    }
   }
 
   render() {
+    var sup =">";
+    var eq = "==";
+    var inf = "<";
+
     return (
       <div className="container jumbotron">
         <div className="row">
@@ -84,7 +132,13 @@ export default class TriggerGenerator extends Component {
             <label htmlFor="TriggerFonctions">Triggering fonction:</label>
             <select className="form-control" id="TriggerFonctions" onChange={ this.FillTriggerList }>
              <option value="0">Température</option>
-             <option value="1">2</option>
+             <option value="1">Humidité</option>
+             <option value="2">Préssion</option>
+             <option value="3">Magnétomètre</option>
+             <option value="4">Gyroscope</option>
+             <option value="5">Accéleromètre</option>
+             <option value="6">Lumière</option>
+             <option value="7">Interrupteur</option>             
             </select>
            </div>
           </div>
@@ -92,34 +146,74 @@ export default class TriggerGenerator extends Component {
             <div className="form-group text-center">
               <label htmlFor="Trigger">Trigger :</label>
               <select className="form-control" id="Trigger" onChange={ this.updateTrigger }>
-                <option value=">"> Suppérieur </option>
-                <option value="<"> Inférieure</option>
-                <option value="=="> Equal</option>
+                <option value=">"> {sup} </option>
+                <option value="<"> {inf}</option>
+                <option value="=="> {eq}</option>
               </select>
               </div>
           </div>
           <div className="col-md-2">
-            <div className="form-group text-center">
-              <label id="TriggerValueLabel" htmlFor="TriggerValue">Value :</label>
+
+            <div className={this.state.captorClass}>
+              <label id="TriggerValueLabel" htmlFor="TriggerValue">Value captor:</label>
               <input type="text" className="form-control" onChange={ this.updateTriggerValue } ></input>
+            </div>
+            
+            <div className={this.state.lightsClass}>
+              <label id="TriggerValueLabel" htmlFor="TriggerValue">Value light :</label>
+              <select className="form-control" onChange={this.updateTriggerValue}>
+                <option value="on">Allumer</option>
+                <option value="off">Eteindre</option>
+              </select>
             </div>
           </div>
           <div className="col-md-3">
             <div className="form-group text-center">
               <label htmlFor="TriggeredFonctions">Triggered fonction:</label>
               <select className="form-control" id="TriggeredFonctions" onChange={ this.updateTriggeredFunction } >
-                <option>Light</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
+                <option value="light">Lumière</option>
+                <option value="socket">Interrupteur</option>
+                <option value="ledmessenger">Led Messenger</option>
               </select>
             </div>
           </div>
           <div className="col-md-2">
-            <div className="form-group text-center">
-              <label id="TriggerValueLabel">Value :</label>
-              <input type="text" className="form-control" onChange={ this.updateChangeValue } ></input>
+            <div className={this.state.lightArgClass}>
+              <label>Value :</label>            
+              <select id="LightArgument" className="form-control" onChange={this.updateChangeValue}>
+              <option value="on">Allumer</option>
+              <option value="off">Eteindre</option>
+              <option value="red">Rouge</option>
+              <option value="yellow">Jaune</option>
+              <option value="green">Verte</option>
+              <option value="blue">Bleue</option>
+              <option value="cyan">Cyan</option>
+              <option value="magenta">Magenta</option>
+
+              </select>
             </div>
+
+            <div className={this.state.socketArgClass}>
+              <label>Value :</label>
+              <select id="SocketArgument" className="form-control" onChange={this.updateChangeValue}>
+              <option value="on">Allumer</option>
+              <option value="off">Eteindre</option>
+              <option value="red">Rouge</option>
+              <option value="yellow">Jaune</option>
+              <option value="green">Verte</option>
+              <option value="blue">Bleue</option>
+              <option value="cyan">Cyan</option>
+              <option value="magenta">Magenta</option>
+              <option value="flash">Flash</option>
+
+              </select>
+            </div>
+
+            <div className={this.state.ledmessengerArgClass}>
+              <label>Value :</label>
+              <input id="LedMessengerArgument" type="text" className="form-control" onChange={ this.updateChangeValue } ></input>
+            </div>
+
           </div>
           <div className="col-md-12 text-center">
             <button className="btn btn-primary" onClick={ this.addTrigger }>Register Trigger</button>
